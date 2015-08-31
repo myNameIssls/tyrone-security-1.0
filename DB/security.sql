ac8b5ef0173ca67bce2daacb7931a8694745c62f
tyrone <791815504@qq.com>
2015-08-31 14:00:38 +0800

    add DB Folder and doc Folder

---
+++ DB/security.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v11.5 (64 bit)
+MySQL - 5.6.18-enterprise-commercial-advanced : Database - security
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`security` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `security`;
+
+/*Table structure for table `security_role` */
+
+DROP TABLE IF EXISTS `security_role`;
+
+CREATE TABLE `security_role` (
+  `ROLE_ID` varchar(36) NOT NULL COMMENT '主键',
+  `ROLE_NAME` varchar(20) NOT NULL COMMENT '角色名称',
+  `ROLE_CODE` varchar(20) NOT NULL COMMENT '角色编码',
+  `STATUS` char(1) NOT NULL COMMENT '状态',
+  PRIMARY KEY (`ROLE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `security_role` */
+
+/*Table structure for table `security_user` */
+
+DROP TABLE IF EXISTS `security_user`;
+
+CREATE TABLE `security_user` (
+  `USER_ID` varchar(36) NOT NULL COMMENT '主键',
+  `LOGIN_NAME` varchar(50) NOT NULL COMMENT '登录名称',
+  `PASSWORD` varchar(32) NOT NULL COMMENT '登录密码',
+  `STATUS` char(1) NOT NULL DEFAULT '0' COMMENT '状态',
+  PRIMARY KEY (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `security_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
