3f48b2714703c2170a126b2be07df0354cd9548a
tyrone <791815504@qq.com>
2015-08-31 18:05:30 +0800

    add the security control annotation

--- src/main/java/cn/tyrone/security/controller/SecurityUserController.java
+++ src/main/java/cn/tyrone/security/controller/SecurityUserController.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.tyrone.security.annotation.RequestPermission;
 import cn.tyrone.security.service.ISecurityUserService;
 
 @Controller
@@ -13,6 +14,7 @@ public class SecurityUserController extends BaseController {
 	
 	@Resource ISecurityUserService securityUserService;
 	
+	@RequestPermission(value = "securityuser-list")
 	@RequestMapping("/list")
 	public String list(){
 		System.out.println("********************");
